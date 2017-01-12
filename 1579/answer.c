66dd0c686a355d099e592e96b69d0d9436d2673f
CDnX <ruby3141@gmail.com>
2017-01-13 00:15:18 +0900

    seems something is wrong

--- 1579/answer.c
+++ 1579/answer.c
@@ -3,34 +3,85 @@
 #include <stdlib.h>
 #include <stdint.h>
 
-void flip(int32_t **map, int32_t n, int32_t m)
+//function prototypes
+void flip_map(map_t map);
+void calcpathv(map_t map, int32_t **path);
+map_t *map_init(int32_t n, int32_t m);
+map_t *map_copy(map_t *map);
+
+typedef struct map_t
+{
+	int32_t **map;
+	int32_t n, m; //same name in the question for height and width.
+	int32_t result;
+} map_t;
+
+int32_t main(void)
+{
+	//todo
+}
+
+map_t *map_init(int32_t n, int32_t m)
+{
+	map_t *temp;
+	int32_t i;
+
+	temp = (map_t *)malloc(sizeof(map_t));
+	temp->map = (int32_t **)malloc(sizeof(int32_t *) * n);
+	i = 0;
+	while(i < n)
+	{
+		*(temp->map+i) = (int32_t *)malloc(m*sizeof(int32_t));
+		i++;
+	}
+	temp->n = n;
+	temp->m = m;
+	temp->result = 0;
+	return temp;
+}
+
+map_t *map_copy(map_t *map)
+{
+	map_t *temp;
+	int32-t i;
+
+	temp = map_init(map.n, map.m);
+	i = 0;
+	while(i < map.n)
+	{
+		memcpy(*(map->map + i), *temp->map + i, m);
+	}
+}
+
+void flip_map(map_t map)
 {
 	int32_t i,j,buf;
+
 	i = 0;
-	while(i<n)
+	while(i<map.n)
 	{
 		j=0;
-		while(j<(m/2))
+		while(j<((map.m+1)/2))
 		{
-			buf = map[i][j];
-			map[i][j] = map[j][i];
-			map[j][i] = buf;
+			buf = map.map[i][j];
+			map.map[i][j] = map.map[n-i-1][m-j-1];
+			map.map[n-i-1][m-j-1] = buf;
 			j++;
 		}
 		i++;
 	}
 }
 
-void calcpathv(int32_t **map, int32_t **path, int32_t n, int32_t m)
+void calcpathv(map_t **map, int32_t **path)
 {
 	int32_t i, j;
 	i = 0;
-	while(i < n)
+	while(i < map.n)
 	{
 		j = 0;
-		while(j < m)
+		while(j < map.m)
 		{
-			path[i][j] = map[i][j];
+			path[i][j] = map.map[i][j];
 			if(i == 0 && j == 0); //structural mistake, but not serious.
 			else if(i == 0) path[i][j] += path[i][j-1];
 			else if(j == 0) path[i][j] += path[i-1][j];
@@ -46,10 +97,10 @@ void calcpathv(int32_t **map, int32_t **path, int32_t n, int32_t m)
 		}
 		i++;
 	}
+	map.result += path[n-1][m-1];
 }
 
-void 
-
+/*
 int32_t main(void)
 {
 	int32_t n, m, i, j, k, result;//same name in the question. i, j, k are temp variables.
@@ -120,3 +171,4 @@ int32_t main(void)
 	}
 	printf("%d\n", result);
 }
+*/
